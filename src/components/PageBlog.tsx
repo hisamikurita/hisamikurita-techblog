8c559a70c6cacc25bfdbea683acba5cefa1c2a44
栗田久海 <kurita_hisami@liginc.co.jp>
2023-11-04 16:43:40 +0900

    update

--- src/components/PageBlog.tsx
+++ src/components/PageBlog.tsx
@@ -1,4 +1,15 @@
-<>
+import { MicroCmsBlogDetailDataType } from "@/libs/types";
+
+export const PageBlog: React.FC<MicroCmsBlogDetailDataType> = (data) => {
+  const { title, body } = data;
+  console.log(title);
+  console.log(title);
+  console.log(title);
+
+  return (
+    <>
       <h1 className="text-[100px]">{title}</h1>
       <p className="text-[100px]">{body}</p>
     </>
+  );
+};
