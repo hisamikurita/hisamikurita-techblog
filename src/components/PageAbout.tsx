f922b3724ccd9a3032d8eec91b3bed7d168eafb1
栗田久海 <kurita_hisami@liginc.co.jp>
2024-01-02 00:52:37 +0900

    update: About

--- src/components/PageAbout.tsx
+++ src/components/PageAbout.tsx
@@ -39,6 +39,28 @@ export const PageAbout: React.FC<MicroCmsAboutDataType> = (data) => {
               </li>
             </ul>
           </div>
+          <div>
+            <h2 className="mt-[100px] font-roboto text-[20px] font-bold">CONTACT</h2>
+            <p className="mt-[40px]">
+              <a href="mailto:kuritahisami@gmail.com">kuritahisami@gmail.com</a>
+            </p>
+            <ul className="mt-[22px] flex items-center gap-[18px]">
+              <li>
+                <a href="https://github.com/hisamikurita" target="_blank" rel="noopener noreferrer" className="flex h-[32px] w-[32px]">
+                  <svg aria-label="Github" viewBox="0 0 16 16" width="16" height="16" className="h-full w-full">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/kurichans_1996" target="_blank" rel="noopener noreferrer" className="flex h-[27px] w-[27px]">
+                  <svg aria-label="x" viewBox="0 0 24 24" width="24" height="24" className="h-full w-full">
+                    <path d="M14.258 10.152L23.176 0h-2.113l-7.747 8.813L7.133 0H0l9.352 13.328L0 23.973h2.113l8.176-9.309 6.531 9.309h7.133zm-2.895 3.293l-.949-1.328L2.875 1.56h3.246l6.086 8.523.945 1.328 7.91 11.078h-3.246zm0 0" />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </Layout>
