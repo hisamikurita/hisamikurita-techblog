04879f04129c70b798c2deba6ae94f637a264df6
栗田久海 <kurita_hisami@liginc.co.jp>
2024-01-29 19:40:12 +0900

    update:

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,41 @@
 import { BaseHeader } from "./BaseHeader";
 import { BaseFooter } from "./BaseFooter";
 import { useHamburgerMenuContext } from "@/context/hamburgerMenu";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useDevice } from "@/hooks/useDevice";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { isMenuOpen } = useHamburgerMenuContext();
+  const { isSp } = useDevice();
+  const router = useRouter();
+  const { isMenuOpen, closeMenu } = useHamburgerMenuContext();
+
+  // ページ遷移時にハンバーガーメニューを閉じる
+  useEffect(() => {
+    router.events.on("routeChangeStart", closeMenu);
+
+    return () => {
+      router.events.off("routeChangeStart", closeMenu);
+    };
+  }, [router, closeMenu]);
+
+  // ハンバーガーメニューが開いているときはスクロール禁止
+  useEffect(() => {
+    if (isMenuOpen) {
+      disableBodyScroll(document.body);
+    } else {
+      enableBodyScroll(document.body);
+    }
+  }, [isMenuOpen]);
+
+  // PC/SPで切り替えが発生した時に初期化処理を実行
+  useEffect(() => {
+    if (!isSp) {
+      closeMenu();
+      enableBodyScroll(document.body);
+    }
+  }, [isSp]);
 
   return (
     <>
