156457c2370354abc434e99c52faeb268f419dbb
Hisami Kurita <hisami.kurita@arsaga.jp>
2025-08-10 02:11:04 +0900

    fix: preview

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,22 +1,32 @@
+import { SOURCE } from "@/constants";
 import { PageIndex } from "@/features/index";
 import { getBlogList } from "@/libs/microcms";
+import { Article } from "@/types";
 import { getDraftKey } from "@/utils/parameters";
 import { useEffect, useState } from "react";
 
 const Index = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([] as Article[]);
 
   useEffect(() => {
     (async () => {
       // 下書き用のパラメーターがある場合は取得して、APIリクエストを行う
       const requestUrl = window.location.href;
       const draftKey = getDraftKey(requestUrl);
-      const data = await getBlogList({ draftKey }, true);
-      setData(data);
+      const microcmsData = await getBlogList({ draftKey }, true);
+
+      const allData: Article[] = [
+        ...(microcmsData.contents?.map((article) => ({
+          ...article,
+          source: SOURCE.MICROCMS,
+        })) || []),
+      ];
+
+      setData(allData);
     })();
   }, []);
 
-  return <PageIndex {...data} />;
+  return <PageIndex data={data} />;
 };
 
 export default Index;
