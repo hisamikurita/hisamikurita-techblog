3ff679baf263280a27ec4cad92cdb97a22ae6155
Hisami Kurita <hisami.kurita@arsaga.jp>
2025-08-10 03:05:51 +0900

    fix: ogp

--- src/components/BaseHead.tsx
+++ src/components/BaseHead.tsx
@@ -1,17 +1,21 @@
 import Head from "next/head";
 import { URL_FAVICON, URL_APPLE_TOUCH_ICON, URL_DEFAULT_OGP } from "@/constants";
 import { BaseHeadDataType } from "@/types";
+import { useRouter } from "next/router";
 
 export const BaseHead: React.FC<BaseHeadDataType> = (data) => {
   const { title, description, thumbnail } = data;
+  const router = useRouter();
   const currentUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://example.com";
   const ogp = thumbnail ? thumbnail : `${currentUrl}${URL_DEFAULT_OGP}`;
+  const canonicalUrl = `${currentUrl}${router.asPath}`;
 
   return (
     <Head>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <link rel="icon" href={URL_FAVICON} />
       <link rel="apple-touch-icon" href={URL_APPLE_TOUCH_ICON} />
+      <link rel="canonical" href={canonicalUrl} />
       <title>{title}</title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
@@ -21,7 +25,7 @@ export const BaseHead: React.FC<BaseHeadDataType> = (data) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogp} />
-      <meta property="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
