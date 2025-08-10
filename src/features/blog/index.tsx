1ce7ddcf4c7372a658f6f6c9be63a7c737263fa9
Hisami Kurita <hisami.kurita@arsaga.jp>
2025-08-11 03:22:15 +0900

    fix: blog

--- src/features/blog/index.tsx
+++ src/features/blog/index.tsx
@@ -4,6 +4,7 @@ import { DateFormatter } from "@/components/DateFormatter";
 import { META_DESCRIPTION, SITE_NAME } from "@/constants";
 import { RichEditor } from "@/components/RichEditor";
 import { parseToc } from "@/utils/parseToc";
+import { ReactSVG } from "react-svg";
 
 export const PageBlogDetail: React.FC<MicroCmsBlogDetailDataType> = (data) => {
   const { title, thumbnail, body, excerpt, publishedAt, updatedAt } = data;
@@ -19,36 +20,42 @@ export const PageBlogDetail: React.FC<MicroCmsBlogDetailDataType> = (data) => {
   return (
     <>
       <BaseHead {...metaData} />
-      <div className="pb-[120px] pt-[60px]">
-        <div className="h-[200px] md:h-[260px] xl:h-[320px]">
-          <img src={`${thumbnail?.url}?fm=webp&q=80`} alt="" width={thumbnail?.width} height={thumbnail?.height} decoding="async" className="h-full w-full object-cover" />
-        </div>
-        <div className="custom-editor-container">
-          <h1 className="mt-[100px] text-[24px] font-bold">{title}</h1>
-          <div className="mt-[16px] grid gap-[8px] md:flex md:gap-[30px]">
-            <div>
-              公開日 : <DateFormatter date={publishedAt} />
+      <div className="pb-10 pt-[60px]">
+        <div className="c-main-container">
+          <h1 className="mt-8 rounded-xl bg-gray-100 px-5 py-4 text-xl font-bold">{title}</h1>
+          <div className="mt-4 grid gap-2 md:flex md:gap-3">
+            <div className="flex items-center gap-2">
+              <ReactSVG src="/icons/calender.svg" aria-hidden className="h-[13px] w-[13px] text-gray-400" />
+              <DateFormatter date={publishedAt} />
             </div>
-            <div>
-              更新日 : <DateFormatter date={updatedAt} />
+            <div className="flex items-center gap-2">
+              <ReactSVG src="/icons/update.svg" aria-hidden className="h-[14px] w-[14px] -rotate-45 text-gray-400" />
+              <DateFormatter date={updatedAt} />
             </div>
           </div>
-          <div className="mt-[82px] bg-[#f5f5f5] p-[20px]">
-            <h2 className="text-[18px] font-bold">目次</h2>
-            <ul className="mt-[16px] grid gap-[6px]">
-              {tocData &&
-                tocData.map((data) => (
-                  <li key={data.id} className="text-[14px] opacity-[0.8]">
-                    <a href={`#${data.id}`}>
-                      <span></span>
-                      {data.title}
-                    </a>
-                  </li>
-                ))}
-            </ul>
-          </div>
-          <div className="mt-[76px]">
-            <RichEditor body={body} />
+          <div className="c-sub-container">
+            <div className="mt-10 aspect-[16/9] w-full overflow-hidden rounded-xl border border-gray-300">
+              <img src={`${thumbnail?.url}?fm=webp&q=80`} alt="" width={thumbnail?.width} height={thumbnail?.height} decoding="async" className="h-full w-full object-cover" />
+            </div>
+            <div className="custom-editor-container">
+              <div className="mt-10 rounded-xl bg-gray-100 px-5 py-4">
+                <h2 className="text-lg font-bold">目次</h2>
+                <ul className="mt-3 grid gap-2">
+                  {tocData &&
+                    tocData.map((data) => (
+                      <li key={data.id} className="text-sm">
+                        <a href={`#${data.id}`}>
+                          <span></span>
+                          {data.title}
+                        </a>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+              <div className="mt-13">
+                <RichEditor body={body} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
