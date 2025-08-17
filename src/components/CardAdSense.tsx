bf49963b57d358fb52f670f4c3b4dab8e474bf93
Hisami Kurita <hisami.kurita@arsaga.jp>
2025-08-17 10:48:35 +0900

    fix:

--- src/components/CardAdSense.tsx
+++ src/components/CardAdSense.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/libs/tailwindMerge";
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 
 declare global {
   interface Window {
@@ -17,8 +17,10 @@ type Props = {
 };
 
 export const CardAdSense = ({ className, googleAdsensePublisherId, adSlot, adFormat, adLayout, fullWidthResponsive }: Props) => {
-  useLayoutEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle ?? []).push({});
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      (window.adsbygoogle = window.adsbygoogle ?? []).push({});
+    }
   }, []);
 
   return <ins className={cn("adsbygoogle", className)} data-ad-client={googleAdsensePublisherId} data-ad-slot={adSlot} data-ad-format={adFormat} data-ad-layout={adLayout} data-full-width-responsive={`${fullWidthResponsive}`} />;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export const Document = () => {
 
   return (
     <Html lang="ja">
-      <Head />
+      <Head>{!!process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_PUBLISHER_ID && <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_PUBLISHER_ID}`} crossOrigin="anonymous" />}</Head>
       <body id={id}>
         <a className="sr-only" href={`#${id}`}>
           本文へ移動
