915aceb6f9d91bb0dc67b58c2577bb3a7b8468ae
Hisami Kurita <hisami.kurita@arsaga.jp>
2025-08-18 02:35:05 +0900

    feat:

--- src/features/index/index.tsx
+++ src/features/index/index.tsx
@@ -25,7 +25,7 @@ export const PageIndex = ({ data }: { data: Article[] }) => {
             {data.map((article: Article, index) => (
               <>
                 {index === 6 && (
-                  <li className="overflow-hidden rounded-lg border-4 border-gray-100">
+                  <li className="overflow-hidden rounded-lg border-4 border-gray-300">
                     <CardAdSense className="block h-full w-full" googleAdsensePublisherId={ADSENSE.googleAdsensePublisherId} adSlot={ADSENSE.adSlot[0]} />
                   </li>
                 )}
